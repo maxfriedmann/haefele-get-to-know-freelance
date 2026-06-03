195393b8b95ccb3736e56bf03297749c0237756b
Max Friedmann <max@smallstack.com>
2026-06-03 10:02:17 +0200

    feat: refactor product detail component to use NgRx store as well

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,9 +1,15 @@
 import { AsyncPipe, CurrencyPipe } from "@angular/common";
 import { Component, inject } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { ActivatedRoute, RouterLink } from "@angular/router";
+import { Store } from "@ngrx/store";
 import { TranslatePipe } from "@ngx-translate/core";
-import { filter, map, switchMap } from "rxjs";
-import { ProductService } from "../../core/services/product.service";
+import type { Product } from "../../core/models/product.model";
+import { ProductsActions } from "../../core/store/products/products.actions";
+import {
+	selectRelatedProducts,
+	selectSelectedProduct,
+} from "../../core/store/products/products.selectors";
 import { PageHeaderComponent } from "../../shared/components/page-header/page-header.component";
 import { ProductGridComponent } from "../../shared/components/product-grid/product-grid.component";
 import { RatingComponent } from "../../shared/components/rating/rating.component";
@@ -21,41 +27,24 @@ import { RatingComponent } from "../../shared/components/rating/rating.component
 	templateUrl: "./product-detail.component.html",
 })
 export class ProductDetailComponent {
-	activatedRoute = inject(ActivatedRoute);
-	productService = inject(ProductService);
+	private readonly route = inject(ActivatedRoute);
+	private readonly store = inject(Store);
 
-	product$ = this.activatedRoute.paramMap.pipe(
-		map((params) => {
-			const productIdParam = params.get("productId");
-			if (!productIdParam) {
-				throw new Error("No productId provided in route parameters");
+	product$ = this.store.select(selectSelectedProduct);
+	relatedProducts$ = this.store.select(selectRelatedProducts);
+
+	constructor() {
+		this.store.dispatch(ProductsActions.loadProducts());
+
+		this.route.paramMap.pipe(takeUntilDestroyed()).subscribe((params) => {
+			const id = Number(params.get("productId"));
+			if (!Number.isNaN(id)) {
+				this.store.dispatch(ProductsActions.selectProduct({ id }));
 			}
-			const productId: number = productIdParam
-				? parseInt(productIdParam, 10)
-				: NaN;
-			if (Number.isNaN(productId)) {
-				throw new Error("Invalid productId provided in route parameters");
-			}
-			return productId;
-		}),
-		switchMap((productId) => this.productService.getById(productId)),
-	);
+		});
+	}
 
-	relatedProducts$ = this.product$.pipe(
-		filter((product) => !!product),
-		switchMap((product) =>
-			this.productService.getAll().pipe(
-				filter((products) => !!products),
-				map((products) =>
-					products.filter(
-						(p) => p.category === product.category && p.id !== product.id,
-					),
-				),
-			),
-		),
-	);
-
-	addToCart(product: any) {
+	addToCart(product: Product) {
 		console.log("Adding to cart:", product);
 	}
 }
